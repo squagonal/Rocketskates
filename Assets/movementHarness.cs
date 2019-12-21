7dfb39282caa951f006e24d02dc777e5eca9a4c3
Chris <squagonal@gmail.com>
2019-12-21 13:19:15 +0000

    player

---
+++ Assets/movementHarness.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementHarness : MonoBehaviour
+{
+    Vector2  movementDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = new Vector2();
+        if (Input.KeyDown("w")) {
+            movementDirection += Vector2.up;
+        }
+        if (Input.KeyDown("s"))
+        {
+            movementDirection += Vector2.down;
+        }
+        if (Input.KeyDown("a"))
+        {
+            movementDirection += Vector2.left;
+        }
+        if (Input.KeyDown("d"))
+        {
+            movementDirection += Vector2.right;
+        }
+        movementDirection.Normalise();
+        Debug.log(movementDirection);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
